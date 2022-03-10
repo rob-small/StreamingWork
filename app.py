9ac77068eacc1a44f742dc997551e9862089e6de
Rob Small <mr_rob_small@yahoo.com>
2022-03-09 19:27:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,21 @@ Created on Tue Mar  8 22:23:34 2022
 @author: mr_ro
 """
 # save this as app.py
-from flask import Flask, escape, request
+from flask import Flask, jsonify
+from mydevice import myDevice
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+
+theDevice =  myDevice(20,5,'dev1')
+
+@app.route('/device_name')
+def get_name():
+    return jsonify(theDevice.name)
+
+@app.route('/device_reading')
+def get_reading():
+    return jsonify(theDevice.take_reading())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=81)
