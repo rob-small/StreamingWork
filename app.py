d579c26ace99f25876f540878266cefe18d7c7f8
Rob Small <mr_rob_small@yahoo.com>
2022-03-12 10:09:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,14 @@
 Created on Tue Mar  8 22:23:34 2022
 
 @author: mr_ro
+
+Expose all methods via web service
+Initialize values via command line
+Make port # configurable by command line
+
 """
 # save this as app.py
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from mydevice import myDevice
 
 app = Flask(__name__)
@@ -21,5 +26,23 @@ def get_name():
 def get_reading():
     return jsonify(theDevice.take_reading())
 
+@app.route('/device_settings',methods = ['GET','PUT'])
+def device_settings():
+    if(request.method == 'GET'):
+        settings = {
+                'mean':theDevice.mean,
+                'stdev':theDevice.stdev
+            }
+        return jsonify(settings)
+    else:
+        pass      
+        if request.is_json:
+            settings = request.get_json()
+            theDevice.mean = float(settings["mean"])
+            theDevice.stdev = float(settings["stdev"])
+            return "success", 201
+        else:
+            return {"error": "Request must be JSON"}, 415
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=81)
+    app.run()
