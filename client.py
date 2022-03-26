82520662aa51b5a9b59df62f4ed8625d18ec0a8b
Rob Small <mr_rob_small@yahoo.com>
2022-03-26 18:51:42 -0400

    Using data.json to define initial configuration

--- client.py
+++ client.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Mar 23 19:35:30 2022
-
+x -Read device list from JSON file
+Set URL and PORT number from command line
+Set file name from command line
 @author: mr_ro
 """
 
@@ -10,37 +12,31 @@ import requests
 import json
 from datetime import datetime
 
-from mydevice import myDevice
-        
 url_add = "http://192.168.56.101:5000/devices"
 headers = {
   'Content-Type': 'application/json'
 }
 
-devices_list = [
-        myDevice(20,5,'dev1'),
-        myDevice(25,1,'dev2'),
-        myDevice(35,3,'dev3'),
-        myDevice(10,2,'dev4')
-    ]
-
+# Opening JSON file
+f = open('data.json')
+ 
+# returns JSON object as a list
+devices_list = json.load(f)
+ 
 for i in devices_list:
-        payload = json.dumps({
-          "name": i.name,
-          "mean": i.mean,
-          "stdev": i.stdev
-        })
-        
+        payload = json.dumps(i)
         response = requests.request("POST", url_add, headers=headers, data=payload)
-
         print(response.text)
+
+# Closing file
+f.close()
 
 url_read = "http://192.168.56.101:5000/devices/reading"
 
 while 1:
     for i in devices_list:
         payload = json.dumps({
-          "name": i.name
+          "name": i["name"]
         })
 
         response = requests.request("GET", url_read, headers=headers, data=payload)
@@ -48,7 +44,7 @@ while 1:
         gauge_out = {}
         gauge_out['timestamp'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f")
         gauge_out['reading'] = response.text
-        gauge_out['name'] =  i.name
+        gauge_out['name'] =  i["name"]
         print(json.dumps(gauge_out), flush=True)
 
     Event().wait(1)
