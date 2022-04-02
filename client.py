8b2db1c0a9aba6f45498512f209eeeba2abebd0a
Rob Small <mr_rob_small@yahoo.com>
2022-04-02 17:35:39 -0400

    Implemented connection to Azure Event Hub

--- client.py
+++ client.py
@@ -12,7 +12,10 @@ import requests
 import json
 from datetime import datetime
 
-url_add = "http://192.168.56.101:5000/devices"
+from azure.eventhub import EventData, EventHubProducerClient
+
+#url_add = "http://192.168.56.101:5000/devices"
+url_add = "http://localhost:5000/devices"
 headers = {
   'Content-Type': 'application/json'
 }
@@ -31,7 +34,23 @@ for i in devices_list:
 # Closing file
 f.close()
 
-url_read = "http://192.168.56.101:5000/devices/reading"
+#url_read = "http://192.168.56.101:5000/devices/reading"
+url_read = "http://localhost:5000/devices/reading"
+
+
+# the event hub name.
+producer = EventHubProducerClient.from_connection_string(conn_str="Endpoint=sb://test-rs-usa.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=eMCAVFmf9giAdC9iaBj0xGitkfokPkY4qOYyFIMoB2g=", eventhub_name="myeventhub")
+
+def pub_message(msg):
+
+    # Create a batch.
+    event_data_batch = producer.create_batch()
+    
+    # Add events to the batch.
+    event_data_batch.add(EventData(json.dumps(msg)))
+    # Send the batch of events to the event hub.
+    producer.send_batch(event_data_batch)
+    print(msg, flush=True)
 
 while 1:
     for i in devices_list:
@@ -45,9 +64,8 @@ while 1:
         gauge_out['timestamp'] = datetime.now().strftime("%m/%d/%Y, %H:%M:%S:%f")
         gauge_out['reading'] = response.text.strip("\n")
         gauge_out['name'] =  i["name"]
-        print(json.dumps(gauge_out), flush=True)
-
+        pub_message(gauge_out)
+        
     Event().wait(1)
-
 
 
