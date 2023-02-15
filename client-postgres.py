fd062f089fc2ffb6787003b9577b882d27efdd95
Rob Small <mr_rob_small@yahoo.com>
2023-02-14 21:11:38 -0500

    Completed postgres client and docker configuration

--- client-postgres.py
+++ client-postgres.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 """
-To do
+CREATE DATABASE device;
+CREATE TABLE reading (
+   device_name text,
+   ts timestamp,
+   value real
+);
 -
 @author: mr_ro
 """
@@ -10,6 +15,7 @@ import requests
 import json
 from datetime import datetime
 import os
+import psycopg2 
 
 # Get service URL. If no environment variable set then look in config file
 url_add = os.environ.get('DeviceServerUrl')
@@ -41,8 +47,23 @@ devices_list = json.load(f)
 # Closing file
 f.close()
 
+# Get database connection settings
+db_host = os.environ.get('devicedb_host')
+db_name = os.environ.get('devicedb_name')
+db_user = os.environ.get('devicedb_user')
+db_pwd = os.environ.get('devicedb_pwd')
+db_port = os.environ.get('devicedb_port')
+
+conn = psycopg2.connect(dbname=db_name, user=db_user, host=db_host, password=db_pwd, port=db_port)
+
+cur = conn.cursor()
+
 def pub_message(msg):
-    print(msg, flush=True)
+    insert_query = "INSERT INTO reading (device_name, ts, value) VALUES ('{0}','{1}',{2})".format(msg['name'],msg['ts'],msg['reading'])
+    cur.execute(insert_query)
+    conn.commit()
+    print(insert_query, flush=True)
+
 
 while 1:
     for i in devices_list:
@@ -59,3 +80,6 @@ while 1:
         pub_message(gauge_out)
         
     Event().wait(1)
+
+cur.close()
+conn.close()
